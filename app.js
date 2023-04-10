296d44b01d8db19ce1a19c631adc9af2062bb6f3
Marat Zimnurov <zimtir@mail.ru>
2023-04-10 00:05:04 +0000

    feat: refactor code

--- app.js
+++ app.js
@@ -1,100 +1,104 @@
-const timeInput = document.getElementById("time");
-const fontInput = document.getElementById("font");
-const sizeInput = document.getElementById("size");
-const bgColorInput = document.getElementById("bgColor");
-const fontColorInput = document.getElementById("fontColor");
-const startBtn = document.getElementById("startBtn");
-const downloadBtn = document.getElementById("downloadBtn");
-const timer = document.getElementById("timer");
+const script = document.createElement("script");
+script.src = "https://rawgit.com/spite/ccapture.js/master/build/CCapture.all.min.js";
 
-function updateTimerStyle() {
-  timer.style.fontFamily = fontInput.value || "Arial";
-  timer.style.fontSize = `${sizeInput.value || 24}px`;
-  timer.style.backgroundColor = bgColorInput.value;
-  timer.style.color = fontColorInput.value;
-}
+script.addEventListener("load", function () {
+  const timeInput = document.getElementById("time");
+  const startTimeInput = document.getElementById("startTime");
+  const fontInput = document.getElementById("font");
+  const sizeInput = document.getElementById("size");
+  const bgColorInput = document.getElementById("bgColor");
+  const fontColorInput = document.getElementById("fontColor");
+  const startBtn = document.getElementById("startBtn");
+  const timer = document.getElementById("timer");
 
-function startTimer() {
-  updateTimerStyle();
-  const startTime = parseInt(timeInput.value, 10);
-  const isCountingUp = startTime === 0;
-  let currentTime = startTime;
+  function updateTimerStyle() {
+    timer.style.fontFamily = fontInput.value || "Arial";
+    timer.style.fontSize = `${sizeInput.value || 24}px`;
+    timer.style.backgroundColor = bgColorInput.value;
+    timer.style.color = fontColorInput.value || "#FFFFFF";
+  }
 
-  timer.textContent = formatTime(currentTime);
-  const intervalId = setInterval(() => {
-    if (isCountingUp) {
-      currentTime++;
-    } else {
-      currentTime--;
-    }
+  function startTimer() {
+    updateTimerStyle();
+    const maxTime = parseInt(timeInput.value, 10);
+    let currentTime = parseInt(startTimeInput.value, 10);
 
     timer.textContent = formatTime(currentTime);
-    if (currentTime === 0 && !isCountingUp) {
-      clearInterval(intervalId);
-    }
-  }, 1000);
-}
+    const intervalId = setInterval(() => {
+      currentTime++;
 
-function formatTime(time) {
-  const minutes = Math.floor(time / 60);
-  const seconds = time % 60;
-  return `${minutes.toString().padStart(2, "0")}:${seconds
-    .toString()
-    .padStart(2, "0")}`;
-}
+      timer.textContent = formatTime(currentTime);
+      if (currentTime >= maxTime) {
+        clearInterval(intervalId);
+      }
+    }, 1000);
+  }
 
-startBtn.addEventListener("click", () => {
-  startTimer();
-  startCapture();
-  downloadBtn.setAttribute("disabled", true);
-});
+  function formatTime(time) {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes
+      .toString()
+      .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+  }
 
-downloadBtn.addEventListener("click", endCapture);
+  startBtn.addEventListener("click", startTimer);
 
-let capturer;
-let canvas;
+  const downloadBtn = document.getElementById("downloadBtn");
+  let capturer;
+  let canvas;
 
-function setupCanvas() {
-  canvas = document.createElement("canvas");
-  canvas.width = 400;
-  canvas.height = 200;
-  document.body.appendChild(canvas);
-}
+  function setupCanvas() {
+    canvas = document.createElement("canvas");
+    canvas.width = 400;
+    canvas.height = 200;
+    document.body.appendChild(canvas);
+  }
 
-function startCapture() {
-  setupCanvas();
-  capturer = new CCapture({
-    format: "webm",
-    framerate: 30,
-    name: "timer-video",
-    timeLimit: parseInt(timeInput.value, 10),
-    verbose: true,
+  function startCapture() {
+    setupCanvas();
+    capturer = new CCapture({
+      format: "webm",
+      framerate: 30,
+      name: "timer-video",
+      timeLimit: parseInt(timeInput.value, 10),
+      verbose: true,
+    });
+    capturer.start();
+  }
+
+  function drawTimer() {
+    const ctx = canvas.getContext("2d");
+    ctx.font = `${sizeInput.value || 24}px ${fontInput.value || "Arial"}`;
+    ctx.fillStyle = bgColorInput.value;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = fontColorInput.value;
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillText(timer.textContent, canvas.width / 2, canvas.height / 2);
+  }
+
+  function endCapture() {
+    capturer.stop();
+    capturer.save();
+    downloadBtn.removeAttribute("disabled");
+    document.body.removeChild(canvas);
+  }
+
+  startBtn.addEventListener("click", () => {
+    startTimer();
+    startCapture();
+    downloadBtn.setAttribute("disabled", true);
   });
-  capturer.start();
-}
 
-function drawTimer() {
-  const ctx = canvas.getContext("2d");
-  ctx.font = `${sizeInput.value || 24}px ${fontInput.value || "Arial"}`;
-  ctx.fillStyle = bgColorInput.value;
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = fontColorInput.value;
-  ctx.textAlign = "center";
-  ctx.textBaseline = "middle";
-  ctx.fillText(timer.textContent, canvas.width / 2, canvas.height / 2);
-}
+  downloadBtn.addEventListener("click", endCapture);
 
-function endCapture() {
-  capturer.stop();
-  capturer.save();
-  downloadBtn.removeAttribute("disabled");
-  document.body.removeChild(canvas);
-}
+  function captureFrame() {
+    drawTimer();
+    capturer.capture(canvas);
+  }
 
-startBtn.addEventListener("click", () => {
-  startTimer();
-  startCapture();
-  downloadBtn.setAttribute("disabled", true);
+  setInterval(captureFrame, 1000 / 30);
 });
 
-downloadBtn.addEventListener("click", endCapture);
+document.head.appendChild(script);
